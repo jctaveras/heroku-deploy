5b5939e754fd14234602c22091c7fb151285f2af
Jean Carlos Taveras <jean.carlos.taveras@x-team.com>
2022-03-23 23:36:25 -0400

    HD-11: Allow Logging Command Results

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { getInput, info, setFailed } from "@actions/core";
 import { exec } from "child_process";
-import { promisify } from "util";
 
 import { herokuActionSetup } from "./lib/scripts";
 import herokuLogin from "./lib/login";
@@ -9,11 +8,19 @@ const herokuAction = herokuActionSetup(getInput('app_name'));
 
 herokuLogin()
   .then(async () => {
-    await promisify(exec)(herokuAction('push'));
+    exec(herokuAction('push'), { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+      if (error) throw new Error(`stderr: ${stderr}`);
+
+      info(`stdout: ${stdout}`);
+    });
     info('Your Docker image was built and pushed to Heroku Container Registry.');
   })
   .then(async () => {
-    await promisify(exec)(herokuAction('release'));
+    await exec(herokuAction('release'), { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+      if (error) throw new Error(`stderr: ${stderr}`);
+
+      info(`stdout: ${stdout}`);
+    });
     info('Your Appliction was deployed successfully.')
   })
   .catch(error => {
