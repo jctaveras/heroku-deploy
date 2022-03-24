9b6c1f42fc03cbd2a94b6b090ca8ad47f01f6296
Jean Carlos Taveras <jean.carlos.taveras@x-team.com>
2022-03-23 23:36:25 -0400

    HD-11: Allow Logging Command Results

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { getInput, info, setFailed } from "@actions/core";
 import { exec } from "child_process";
-import { promisify } from "util";
 
 import { herokuActionSetup } from "./lib/scripts";
 import herokuLogin from "./lib/login";
@@ -9,12 +8,22 @@ const herokuAction = herokuActionSetup(getInput('app_name'));
 
 herokuLogin()
   .then(async () => {
-    await promisify(exec)(herokuAction('push'));
-    info('Your Docker image was built and pushed to Heroku Container Registry.');
+    const child = exec(herokuAction('push'), { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+      if (error) throw new Error(`stderr: ${stderr}`);
+
+      info(`stdout: ${stdout}`);
+    });
+
+    child.on('close', () => info('Your Docker image was built and pushed to Heroku Container Registry.'));
   })
   .then(async () => {
-    await promisify(exec)(herokuAction('release'));
-    info('Your Appliction was deployed successfully.')
+    const child = exec(herokuAction('release'), { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+      if (error) throw new Error(`stderr: ${stderr}`);
+
+      info(`stdout: ${stdout}`);
+    });
+
+    child.on('close', () => info('Your Appliction was deployed successfully.'));
   })
   .catch(error => {
     setFailed(`Something went wrong building your image. [Error]: ${(error as Error).message}`);
